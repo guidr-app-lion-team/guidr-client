8c6064caca5573a19503b6caca30649f18adb527
Timothy Hoang <timh1203@gmail.com>
2019-02-04 17:37:32 -0600

    Add logic for Register for handleChange and handleSubmit()

--- guidr-client/src/components/Register.js
+++ guidr-client/src/components/Register.js
@@ -1,38 +1,38 @@
 import React from 'react'
 
-export default function Register() {
+export default function Register(props) {
   return (
     <div className="w-full mx-auto flex flex-col justify-center h-screen">
-      <form onSubmit={() => console.log(`submitting`)} className="w-4/5 mx-auto bg-white shadow-md px-8 pt-6 mb-4 rounded">
-        <h1 className>Registration</h1>
+      <form onSubmit={(e) => props.handleSubmit(e)} className="w-4/5 mx-auto bg-white shadow-md px-8 pt-6 mb-4 rounded">
+        <h1>Registration</h1>
         <div className="flex justify-around rounded px-8 pt-6 pb-8 mb-4">
           <div className="w-5/6">
             <div className="mb-4 w-4/5 mx-auto">
               {/* <label className="block text-grey-darker text-sm font-bold mb-2 text-center" htmlFor="username">
                 Username
               </label> */}
-              <input onChange={() => console.log('changing')} className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline text-center" id="username" type="text" name="username" placeholder="Username" required />
+              <input onChange={(e) => props.handleChange(e)} className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline text-center" id="username" type="text" name="username" placeholder="Username" required />
             </div>
 
             <div className="mb-4 w-4/5 mx-auto">
               {/* <label className="text-left block text-grey-darker text-sm font-bold mb-2" htmlFor="text">
                 Email
             </label> */}
-              <input onChange={() => console.log('changing')} className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline text-center" id="email" type="email" name="email" placeholder="Email" required />
+              <input onChange={(e) => props.handleChange(e)} className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline text-center" id="email" type="email" name="email" placeholder="Email" required />
             </div>
 
             <div className="mb-4 w-4/5 mx-auto">
               {/* <label className="block text-grey-darker text-sm font-bold mb-2 text-left" htmlFor="username">
                 Password
               </label> */}
-              <input onChange={() => console.log('changing')} className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline text-center" id="password" type="password" name="password" placeholder="Password" required />
+              <input onChange={(e) => props.handleChange(e)} className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline text-center" id="password" type="password" name="password" placeholder="Password" required />
             </div>
 
             <div className="mb-4 w-4/5 mx-auto">
               {/* <label className="block text-grey-darker text-sm font-bold mb-2 text-left" htmlFor="username">
                 Retype Password
               </label> */}
-              <input onChange={() => console.log('changing')} className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline text-center" id="passwordCheck" type="password" name="passwordCheck" placeholder="Retype Password" required />
+              <input onChange={(e) => props.handleChange(e)} className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline text-center" id="password2" type="password" name="password2" placeholder="Retype Password" required />
             </div>
           </div>
 
@@ -41,26 +41,26 @@ export default function Register() {
               {/* <label className="text-left block text-grey-darker text-sm font-bold mb-2" htmlFor="text">
                 Name
               </label> */}
-              <input onChange={() => console.log('changing')} className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline text-center" id="name" type="text" name="name" placeholder="Full Name" />
+              <input onChange={(e) => props.handleChange(e)} className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline text-center" id="name" type="text" name="name" placeholder="Full Name" />
             </div>
 
             <div className="mb-4 w-4/5 mx-auto">
               {/* <label className="text-left block text-grey-darker text-sm font-bold mb-2" htmlFor="textarea">
                 Location
               </label> */}
-              <input onChange={() => console.log('changing')} className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline text-center" id="location" type="text" name="location" placeholder="Location" />
+              <input onChange={(e) => props.handleChange(e)} className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline text-center" id="location" type="text" name="location" placeholder="Location" />
             </div>
 
             <div className="mb-4 w-4/5 mx-auto">
               {/* <label className="text-left block text-grey-darker text-sm font-bold mb-2" htmlFor="textarea">
                 Bio
               </label> */}
-              <textarea onChange={() => console.log('changing')} className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline text-center" id="bio" type="text" name="bio" placeholder="Bio" maxLength="400" />
+              <textarea onChange={(e) => props.handleChange(e)} className="shadow appearance-none border rounded w-full py-2 px-3 text-grey-darker leading-tight focus:outline-none focus:shadow-outline text-center" id="bio" type="text" name="bio" placeholder="Bio" maxLength="400" />
             </div>
 
             <div className="mb-4 w-4/5 mx-auto">
               <label className="text-center block text-grey-darker text-sm font-bold mb-2" htmlFor="text">
-                Professional? <input onChange={() => console.log('changing')} className="shadow" id="professional" type="checkbox" name="professional" placeholder="Professional" />
+                Professional? <input onChange={(e) => props.handleChange(e)} className="shadow" id="professional" type="checkbox" name="professional" placeholder="Professional" />
               </label>
             </div>
           </div>
--- guidr-client/src/views/RegisterView.js
+++ guidr-client/src/views/RegisterView.js
@@ -7,11 +7,44 @@ export class RegisterView extends Component {
   //   static propTypes = {
   //     prop: PropTypes
   //   }
+  state = {
+    username: "",
+    email: "",
+    password: "",
+    password2: "",
+    name: "",
+    location: "",
+    bio: "",
+    professional: false
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    if (this.state.password1 === this.state.password2) {
+      //send the data via axios
+    }
+    else {
+      alert("You're passwords don't match.")
+    }
+  }
+
+  handleChange = (e) => {
+    console.log(e.target.name, e.target.value)
+    if (e.target.name === "professional") {
+      this.setState({ professional: !this.state.professional })
+    }
+    else {
+      this.setState({ [e.target.name]: e.target.value })
+    }
+  }
 
   render() {
     return (
       <div className="w-screen login-screen">
-        <Register />
+        <Register
+          handleSubmit={this.handleSubmit}
+          handleChange={this.handleChange}
+        />
       </div>
     )
   }
